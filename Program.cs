21836e31246008d162a70c19d437e2f15c231c40
afolayanolamiderahmon <afolayan.olamiderahmon95@gmail.com>
2021-11-15 02:55:22 -0800

    created a new csharp project and added a line of code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace gitclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("your name is Adewale");
+        }
+    }
+}
